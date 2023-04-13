37541375df6521b20cb24db6db108216cf034410
krp77 <krp77@mail.ru>
2023-04-13 20:54:53 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.io.File;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can    always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(getF(new File("c:/Intel")));
+
+
     }
+static public long getF(File folder)
+{if (folder.isFile())
+{return folder.length();}
+    long sum=0;
+File [] files=folder.listFiles();
+
+for (File f:files)
+{sum=sum=getF(f);}
+
+
+
+return  sum;}
+
+
+
 }
