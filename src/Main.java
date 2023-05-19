fefc6e3e2828e84d2d85ea640f824075a1f7e6c3
krp77 <krp77@mail.ru>
2023-05-19 14:32:16 +0300

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        File file=new File("C:/");
+        String[] fL=file.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name.contains("s"))
+                {return  true;}
+                return false;
+            }
+        });
+        Arrays.stream(fL).forEach(f-> System.out.println(f));
 
-        System.out.println(getF(new File("c:/Intel")));
-
+        System.out.println(getF(new File("c:/")));
 
     }
 static public long getF(File folder)
@@ -17,11 +28,21 @@ static public long getF(File folder)
 File [] files=folder.listFiles();
 
 for (File f:files)
-{sum=sum=getF(f);}
+{sum=sum+getF(f);}
 
 
 
 return  sum;}
+
+    File file=new File("C:/");
+    String[] fL=file.list(new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+            if (name.contains("s"))
+            {return  true;}
+            return false;
+        }
+    });
 
 
 
